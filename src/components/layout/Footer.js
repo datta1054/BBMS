1cf96773744396781d7bba1a97a15076a802f34c
Gurudatta Kamalakar Gadde <107559788+datta1054@users.noreply.github.com>
2023-01-04 08:02:58 +0530

    Add files via upload
    frontend uploaded

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../assets/css/Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <a href="/login/emp">EMP LOGIN</a>
+    </div>
+  );
+};
+
+export default Footer;
